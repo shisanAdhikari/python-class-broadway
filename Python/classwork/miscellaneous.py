c277ebbb7f6749d53e953b2de83d88db49b26048
Shisan Adhikari <shisanadhikari15@gmail.com>
2022-12-21 07:49:23 +0545

    this is my another new commit

---
+++ Python/classwork/miscellaneous.py
@@ -0,0 +1,25 @@
+"""
+def sum(list, target):
+    array = []
+    for i in range(list):
+
+        for j in range(i, len(list)):
+            a = list[i] + list[j]
+
+            if list[i] == list[j]:
+                continue
+
+
+            elif list[i] + list[j] == target:
+            return (list[i], list[j])
+
+    return []
+
+
+result = sum(list=[3, 5, -4, 8, 11, 1, -1, 6], target=10)
+print(result)
+
+"""
+
+print("hello")
+
